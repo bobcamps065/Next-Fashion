5a0c020f7529c23eccf235477f5ae372feafbca1
bobcamps065 <brunocamps1@hotmail.com>
2022-12-29 12:04:38 -0300

    Meu primeiro commit

---
+++ Home.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+       <link rel="stylesheet" type="text/css" href="main.css">
+    <title>Next Fashion</title>
+
+</head>
+<body>
+  <img class="car" id="car" src="imagem/bag1.png" alt="">
+    <h1  id="h1">Next Fashion</h1>
+  
+      <h5 style="text-align: center; letter-spacing: 2px;">www.nextfashion.com</h5>
+ 
+<!-- ------------------------------------------------------------------------------ -->
+     <div id="ul" class="ul">
+        <ul>
+          <li>Man</li>  
+          <li>Woman</li>  
+          <li>Adolescent</li>  
+          <li>Baby</li>  
+          <li>Child</li>  
+          <li>Best age</li>  
+        </ul>
+     </div>
+     
+<!-- ------------------------------------------------------------------------------ -->
+      <div class="item1">
+        <div class="card">
+           <h3 class="title1">Sale 50% Off</h3>
+            <img class="item2" src="imagem/camisa1.png" alt="">
+             <p class="title2"><strong>Price</strong> $00,00</p>
+          <button id="btn" type="submit">Buy Now</button><br>
+          <br><button id="btn" type="submit">Add</button><br>
+            <span id="star" class="material-symbols-outlined">star</span>   
+            <span id="star" class="material-symbols-outlined">star</span>   
+            <span id="star" class="material-symbols-outlined">star</span>
+            <span id="star" class="material-symbols-outlined">star_rate_half</span>   
+        </div>
+
+          <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+              <img class="item2" src="imagem/camisa2.png" alt="">
+               <p class="title2"><strong>Price</strong> $00,00</p>
+              <button id="btn" type="submit">Buy Now</button><br>
+              <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+          </div>
+
+          <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+              <img class="item2" src="imagem/camisa3.png" alt="">
+               <p class="title2"><strong>Price</strong> $00,00</p>
+            <button id="btn" type="submit">Buy Now</button><br>
+            <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>      
+          </div>
+
+          <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+              <img class="item2" src="imagem/camisa4.png" alt="">
+               <p class="title2"><strong>Price</strong> $00,00</p>
+            <button id="btn" type="submit">Buy Now</button><br>
+            <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>      
+          </div>
+      </div><br><hr>
+<!-- ------------------------------------------------------------------------------ -->
+       <div class="item1">
+        <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+            <img class="item2" src="imagem/casaco1.jpg" alt="">
+            <p class="title2"><strong>Price</strong> $00,00</p>
+            <button id="btn" type="submit">Buy Now</button><br>
+            <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+        </div>
+
+        <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+            <img class="item2" src="imagem/casaco2.jpg" alt="">
+            <p class="title2"><strong>Price</strong> $00,00</p>
+            <button id="btn" type="submit">Buy Now</button><br>
+            <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+        </div>
+
+        <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+            <img class="item2" src="imagem/casaco3.jpg" alt="">
+            <p class="title2"><strong>Price</strong> $00,00</p>
+            <button id="btn" type="submit">Buy Now</button><br>
+            <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+        </div>
+
+        <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+            <img class="item2" src="imagem/casaco4.jpg" alt="">
+            <p class="title2"><strong>Price</strong> $00,00</p>
+            <button id="btn" type="submit">Buy Now</button><br>
+            <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+        </div>
+       </div><br><hr>
+            <div class="item1">
+              
+              <div class="card">
+                <h3 class="title1">Sale 50% Off</h3>
+                <img class="item2" src="imagem/mulher1.jpg" alt="">
+                <p class="title2"><strong>Price</strong> $00,00</p>
+                <button id="btn" type="submit">Buy Now</button><br>
+                <br><button id="btn" type="submit">Add</button><br>
+                    <span id="star" class="material-symbols-outlined">star</span>   
+                    <span id="star" class="material-symbols-outlined">star</span>   
+                    <span id="star" class="material-symbols-outlined">star</span>
+                    <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+            </div>
+            <div class="card">
+              <h3 class="title1">Sale 50% Off</h3>
+              <img class="item2" src="imagem/mulher2.jpg" alt="">
+              <p class="title2"><strong>Price</strong> $00,00</p>
+              <button id="btn" type="submit">Buy Now</button><br>
+              <br><button id="btn" type="submit">Add</button><br>
+                  <span id="star" class="material-symbols-outlined">star</span>   
+                  <span id="star" class="material-symbols-outlined">star</span>   
+                  <span id="star" class="material-symbols-outlined">star</span>
+                  <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+          </div>
+          <div class="card">
+            <h3 class="title1">Sale 50% Off</h3>
+            <img class="item2" src="imagem/mulher3.jpg" alt="">
+            <p class="title2"><strong>Price</strong> $00,00</p>
+            <button id="btn" type="submit">Buy Now</button><br>
+            <br><button id="btn" type="submit">Add</button><br>
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>   
+                <span id="star" class="material-symbols-outlined">star</span>
+                <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+        </div>
+        <div class="card">
+          <h3 class="title1">Sale 50% Off</h3>
+          <img class="item2" src="imagem/mulher4.jpg" alt="">
+          <p class="title2"><strong>Price</strong> $00,00</p>
+          <button id="btn" type="submit">Buy Now</button><br>
+          <br><button id="btn" type="submit">Add</button><br>
+              <span id="star" class="material-symbols-outlined">star</span>   
+              <span id="star" class="material-symbols-outlined">star</span>   
+              <span id="star" class="material-symbols-outlined">star</span>
+              <span id="star" class="material-symbols-outlined">star_rate_half</span>       
+      </div>        
+        </div>
+            
+         <div class="text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+          sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+          software like Aldus PageMaker including versions of Lorem Ipsum. &#8212;
+         </div>
+    <footer>
+        Next Fashion <h4>www.nextfashion.com</h4>
+    </footer>
+   <script id="footer" src="css.js"></script>
+</body>
+</html>
---
+++ css.js
@@ -0,0 +1,33 @@
+const  h1 = document.querySelector("h1");
+h1.style.color = "white"
+h1.style.background = "darkred"
+h1.style.textAlign = "center"
+h1.style.padding = "10px"
+h1.style.position = "sticky"
+h1.style.position = "-webkit-sticky"
+h1.style.top = "0"
+h1.style.zIndex = "3"
+//====================================================
+const ulLi = document.getElementById("ul");
+ulLi.style.listStyleType = "none"
+// ulLi.style.background = "lightgray"
+ulLi.style.color= "darkred"
+//====================================================
+const  footer = document.querySelector("footer");
+footer.style.color = "white"
+footer.style.background = "darkred"
+footer.style.textAlign = "center"
+footer.style.padding = "20px"
+footer.style.marginTop ="10px"
+//====================================================
+const carPay = document.getElementById('car');
+// carPay.style.width = "40px"
+carPay.style.margin = "10px"
+carPay.style.cursor = "pointer"
+carPay.style.right = "0"
+carPay.style.bottom = "60px"
+carPay.style.position = "fixed"
+carPay.style.backgroundColor = "goldenrod"
+carPay.style.border = "solid 3px"
+carPay.style.zIndex = "1"
+carPay.style.filter = "drop-shadow(0px 0px 3px #007185)"
